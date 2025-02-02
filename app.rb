5d224388219858ed8690eaa4e53bdd75f362be78
jessie5557 <ychen38@chicagobooth.edu>
2025-02-02 23:20:54 +0000

    homepage works

--- app.rb
+++ app.rb
@@ -2,11 +2,9 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  erb(:homepage)
 end
+
 
 get("/rock") do
   moves = ["rock","paper","sciccors"]
@@ -15,11 +13,45 @@ get("/rock") do
   if @comp_move == "rock"
     @outcome = "tied"
   elsif @comp_move == "paper"
+    @outcome = "lost"
+  else
+    @outcome = "won"
+  end
+
+  erb(:zebra_r)
+
+end
+
+
+get("/scissors") do
+  moves = ["rock","paper","sciccors"]
+  @comp_move = moves.sample
+
+  if @comp_move == "sciccors"
+    @outcome = "tied"
+  elsif @comp_move == "paper"
     @outcome = "won"
   else
     @outcome = "lost"
   end
 
-  erb(:zebra)
+  erb(:zebra_s)
+
+end
+
+
+get("/paper") do
+  moves = ["rock","paper","sciccors"]
+  @comp_move = moves.sample
+
+  if @comp_move == "paper"
+    @outcome = "tied"
+  elsif @comp_move == "rock"
+    @outcome = "lost"
+  else
+    @outcome = "won"
+  end
+
+  erb(:zebra_p)
 
 end
